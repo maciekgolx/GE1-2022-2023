ce0f5f07136344741b59d43cfff5b0ab5b7d6f6c
maciekgolx <65551224+maciekgolx@users.noreply.github.com>
2022-10-05 16:52:21 +0200

    Update

--- GE1 Examples 2022/Assets/RotateMe.cs
+++ GE1 Examples 2022/Assets/RotateMe.cs
@@ -9,11 +9,12 @@ public class RotateMe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
     }
 }
