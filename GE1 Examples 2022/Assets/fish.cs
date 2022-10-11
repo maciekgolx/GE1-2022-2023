ed91601177aaa3de08473c61c5c260f6da04c33d
maciekgolx <65551224+maciekgolx@users.noreply.github.com>
2022-10-11 20:21:44 +0200

    update

---
+++ GE1 Examples 2022/Assets/fish.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fish : MonoBehaviour
+{
+    public Transform head;
+    public Transform tail;
+    [Range(0.0f, 5.0f)]
+    public float frequency = 0.5f;
+    public float headAmplitude = 40;
+    public float tailAmplitude = 60;
+    public float theta = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float headang = 0;
+        float tailang = 0;
+        theta+= Time.deltaTime * frequency;
+        headang= Mathf.Sin(theta) * headAmplitude;
+        tailang= Mathf.Sin(theta) * tailAmplitude;
+        head.transform.localRotation = Quaternion.AngleAxis(headang,Vector3.forward ) ;
+        tail.transform.localRotation = Quaternion.AngleAxis(tailang,Vector3.forward ); 
+            
+    }
+}
