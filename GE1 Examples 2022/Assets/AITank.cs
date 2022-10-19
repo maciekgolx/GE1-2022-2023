bb4f2300205c863fa5be2855d6880f483bcd6566
maciekgolx <65551224+maciekgolx@users.noreply.github.com>
2022-10-19 19:17:16 +0200

    Update

--- GE1 Examples 2022/Assets/AITank.cs
+++ GE1 Examples 2022/Assets/AITank.cs
@@ -16,12 +16,15 @@ public class AITank : MonoBehaviour {
     {
         if (!Application.isPlaying)
         {
-            // Task 1
-            // Put code here to draw the gizmos
-            // Use sin and cos to calculate the positions of the waypoints 
-            // You can draw gizmos using
-            // Gizmos.color = Color.green;
-            // Gizmos.DrawWireSphere(pos, 1);
+           float theta = (Mathf.PI * 2.0f) / numWaypoints;
+            for(int i = 0 ; i < numWaypoints ; i ++)
+            {
+                float angle = theta * i;
+                Vector3 ps = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+                ps = transform.TransformPoint(ps);
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(ps, 1); 
+            }
         }
     }
 
@@ -30,6 +33,14 @@ public class AITank : MonoBehaviour {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
+        float theta = (Mathf.PI * 2.0f) / numWaypoints;
+        for(int i = 0 ; i < numWaypoints ; i ++)
+        {
+            float angle = theta * i;
+            Vector3 ps = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+            ps = transform.TransformPoint(ps);
+            waypoints.Add(ps); 
+        }
     }
 
     // Update is called once per frame
@@ -37,13 +48,44 @@ public class AITank : MonoBehaviour {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
-
+        Vector3 ps = transform.position;
+        Vector3 Next = waypoints[current] - ps;
+        float dist = Next.magnitude;
+        if (dist < 1)
+        {
+            current = (current + 1) % waypoints.Count;
+        }
+        Vector3 direction = Next / dist;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Next, Vector3.up), 270 * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, waypoints[current], Time.deltaTime);
+    
 
         // Task 4
         // Put code here to check if the player is in front of or behine the tank
+         Vector3 Player = player.position - transform.position;
+        float dot = Vector3.Dot(transform.forward,Player.normalized);
+        float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        
+        if (dot < 0.5)
+        {
+            GameManager.Log("Tank is behind");       
+        }
+        else
+        {
+            GameManager.Log("Tank is in front"); 
+        }
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
-        GameManager.Log("Hello from th AI tank");
+
+        if (angle < 0.5)
+        {
+            GameManager.Log("Tank is in the fov");       
+        }
+        else
+        {
+            GameManager.Log("Tank is not in fov"); 
+        }
+
     }
 }
